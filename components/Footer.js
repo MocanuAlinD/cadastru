1d3f189ab931ee52deaebd9028ad64969ef11257
Mocanu Alin <alin_ngt@yahoo.com>
2022-03-28 11:21:59 +0300

    Added footer

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/footer.module.scss'
+
+const Footer = () => {
+  return (
+    <div className={styles.container + " d-flex justify-content-center align-items-center"}>Footer</div>
+  )
+}
+
+export default Footer
---
+++ pages/servicii.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Servicii from '../components/Servicii'
+
+const servicii = () => {
+  return (
+    <Servicii />
+  );
+};
+
+export default servicii;
